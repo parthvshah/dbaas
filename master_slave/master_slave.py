645cb79d7d6f2fc80e8a87e960e0063489970419
ashrith_sridhar <45114939+ashrithsridhar@users.noreply.github.com>
2020-04-26 22:24:53 +0530

    Update master_slave.py

--- master_slave/master_slave.py
+++ master_slave/master_slave.py
@@ -182,11 +182,11 @@ import time
 
 def my_listener(state):		#just listening to the state of connection
 	if state == KazooState.LOST:
-        	print "Connection Lost/Closed"
+        	print("Connection Lost/Closed")
 	elif state == KazooState.SUSPENDED:
-        	print "Connection Suspended"
+        	print("Connection Suspended")
 	else:
-        	print "Welcome to the Zookeeper"
+        	print("Welcome to the Zookeeper")
 
 def with_zeros(node,length):	# while converting the node ids to integar, the leading zeros disappears. They are being added back here
 	zeros_node=str(node).zfill(length)
@@ -196,22 +196,22 @@ def previous(children,mynode,length):	# Find the next lowest node to the current
 	new_children=pop_leader_node(children)
 	children1=map(int, new_children)	# Converting values to integar as I was getting an error while using max()
 	if int(mynode)==min(children1):
-		print "You are the smallest '%s' so become the leader now!!" %mynode
+		print("You are the smallest '%s' so become the leader now!!" %mynode)
 		p_node=mynode
 	else:
 		my_node=max(t for t in children1 if t<int(mynode))
-		print "Current node id is %s " %mynode
+		print("Current node id is %s " %mynode)
 		p_node=with_zeros(my_node,length)
-		print "Previous node id is %s" %p_node
+		print("Previous node id is %s" %p_node)
 	return p_node
 
 def smallest(length):	# To find the smallest node 
 	children=zk.get_children("/leader")
 	children_int= map(int, children)
-#	print "children in smallest %s" %children_int
+#	print("children in smallest %s" %children_int)
 	node=min(int(s) for s in children_int)
 	s_node=with_zeros(node,length)	
-#	print "Smallest node id is %s" %s_node
+#	print("Smallest node id is %s" %s_node)
 	return s_node
 
 def pop_leader_node(children):	#to discard the leader_node temporarily for for processing
@@ -229,11 +229,11 @@ def leader(mynode, children, length):
 	p_node=previous(children,mynode,length)	# Let's see what the next smallest node is	
 	if zk.exists("/leader/leader_node"):    # Check to see if the leader node exists
 		data,stat=zk.get("/leader/leader_node") #extract the information about current leader
-		print "Current Leader is %s" %data
+		print("Current Leader is %s" %data)
 	else:   # If no leader node is present
 		s_node=smallest(length)        # Find smallest node in the list
 		if int(mynode)==int(s_node):    # If our node is the smallest
-			print "Hey you are the leader!! %s" %mynode
+			print("Hey you are the leader!! %s" %mynode)
 			zk.create("/leader/leader_node", mynode.encode('utf8'), ephemeral= True)        #as our node is the leader so become one
 		else:	# Our node is not the smallest so just move towards the watch part
 			pass
@@ -245,7 +245,7 @@ zk.start()
 
 if zk.exists("/leader"):
 	first=0		# assigning this number just to find out if this is first time node is entering the leader election or not
-	print "leader node is present"
+	print("leader node is present")
 
 	
 	while True:
@@ -260,12 +260,12 @@ if zk.exists("/leader"):
 			children=zk.get_children("/leader")
 			length=len(mynode)
 			p_node=previous(children,mynode,length)
-			print p_node			
+			print(p_node)			
 			
-			print " Watching the next smaller node"
+			print(" Watching the next smaller node")
 
 		#time.sleep(5)
 
 else:
-	print "No leader election"
+	print("No leader election")
 	
