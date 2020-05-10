7be6526028a50d5dd5784146f8c8d6f502f2d3d6
Parth Shah <parthvshah@gmail.com>
2020-05-10 13:19:39 +0530

    Port change

--- orchestrator/run.sh
+++ orchestrator/run.sh
@@ -2,4 +2,4 @@
 
 exec python -u zoo.py &
 exec python -u scale_watch.py &
-exec python -u orchestrator.py runserver --host 0.0.0.0 --port 5000
+exec python -u orchestrator.py runserver --host 0.0.0.0 --port 80
